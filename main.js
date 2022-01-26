653a97a164369a01e82efea1a2d79928ac6bb620
Matthew Douglas <matthew@bytesizecoder.ca>
2022-01-26 15:28:25 -0500

    Add new logo!

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function navigate(url) {
     window.open(url, '_blank').focus();
 }
+
+const observer = new IntersectionObserver((enteries) => {
+    // Loop over the entries
+    enteries.forEach((entry) => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+            // Add the animation class
+            entry.target.classList.add('type-animate');
+        }
+    });
+});
+
+observer.observe(document.querySelector('#about-me-title'));
