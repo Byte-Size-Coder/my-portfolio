8dcdf6917185af40930bf93271a2f680ebd4229b
Matthew Douglas <matthew@bytesizecoder.ca>
2022-01-28 10:49:26 -0500

    Add chart for skills to help with responsiveness

--- main.js
+++ main.js
@@ -14,3 +14,58 @@ const observer = new IntersectionObserver((enteries) => {
 });
 
 observer.observe(document.querySelector('#about-me-title'));
+
+var skills = [
+    'React',
+    'JavaScript',
+    'TypeScript',
+    'HTML/CSS',
+    'Material-UI',
+    'Unity',
+    'C#',
+    'Firebase',
+    'UE4',
+    'NodeJS',
+    'SQL',
+    'React Native',
+    'Git',
+    'MongoDB',
+    'C++',
+];
+
+var values = [95, 90, 90, 85, 85, 85, 80, 80, 75, 75, 70, 70, 70, 65, 60];
+
+var myChart = new Chart('myChart', {
+    type: 'horizontalBar',
+    data: {
+        labels: skills,
+        datasets: [
+            {
+                backgroundColor: '#fe5f55',
+                data: values,
+                label: '',
+            },
+        ],
+    },
+    options: {
+        scales: {
+            xAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
+        legend: {
+            display: false,
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                },
+            },
+        },
+    },
+});
